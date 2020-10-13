56f827d9ad88fcfebd103fc595522903ecb408bd
Victor Nystad <vnys@equinor.com>
2020-10-13 22:50:06 +0200

    rename files

---
+++ src/foo.ts
@@ -0,0 +1,12 @@
+type Address = [
+  streetName: string,
+  streetNumber: number,
+  postalCode: number,
+  city: string,
+];
+
+const myAddress: Address = ['Storskarvegen', 12, 5305, 'Florvåg'];
+
+console.log(myAddress);
+
+
